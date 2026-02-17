5e1cd86e46aab9f0c5d496ac9b8d6fbe0c45e083
amirali <amir.boeing797@gmail.com>
2026-02-17 17:15:49 +0330

    3D se block added

--- attention/SE_block.py
+++ attention/SE_block.py
@@ -1,5 +1,5 @@
 from tensorflow import keras
-from keras.layers import Layer , GlobalAveragePooling2D , Dense , Multiply
+from keras.layers import Layer , GlobalAveragePooling2D , Dense , Multiply , GlobalAveragePooling3D
 #---
 class se(Layer) :
     def __init__(self , reduction_ratio=16 , out="channel_scaled") : #constructor
@@ -25,8 +25,30 @@ class se(Layer) :
             return score_out
     def call(self , tensor) :
         return self._se_block(tensor)
-        
-        
+class se3D(Layer) :
+    def __init__(self , reduction_ratio=16 , out="channel_scaled") :
+        super().__init__()
+        self.rr      = reduction_ratio
+        self.out     = out
+        self.multily = Multiply()
+        self.GAP3D   = GlobalAveragePooling3D(keepdims=True)
+    def build(self , input_shape) :
+        self.hidden = Dense(max(input_shape[-1]//self.rr , 1) , activation="relu" , kernel_initializer="he_normal")
+        if self.out == "channel_scaled" or self.out == "channel_weights" :
+            self.score = Dense(input_shape[-1] , activation="sigmoid")
+        elif self.out == "RAW" :
+           self.score = Dense(input_shape[-1] , activation=None)
+    def _se_block(self , tensor) :
+        GAP_out    = self.GAP(tensor)
+        hidden_out = self.hidden(GAP_out)
+        score_out  = self.score(hidden_out)
+        #--
+        if self.out == "channel_scaled" :
+            return self.multiply([score_out , tensor])
+        elif self.out == "channel_weights" or self.out == "RAW":
+            return score_out
+    def call(self , tensor) :
+        return self._se_block(tensor)
         
 
 
